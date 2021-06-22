33a4702b2fa0ff46af12a8034dc7da9fde043ab0
Hama <hmms@gmail.com>
2021-06-22 13:05:18 +0200

    user controller added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def index
+    @current_user = current_user
+    @currect_answer = Game.where("user_id  = #{@current_user.id} AND has_guessed = true").count
+    @false_answer = Game.where("user_id  = #{@current_user.id} AND has_guessed = false").count
+    @score = (@currect_answer -   @false_answer) * 10
+  end
+end
