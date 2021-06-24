da051ccadfc4bcfc5b710bba62d1fee837142354
Hama <hmmshl@gmail.com>
2021-06-24 16:04:57 +0200

    home routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # resources :games, only: [:index, :show, :update]
   # root to: "games#index"
-  root to: "games#index", as: 'games'
+  root to: "games#home", as: 'home'
+  get 'game', to: "games#index", as: 'games'
   get 'game/:id/result', controller: 'games', action: :result, as: 'game'
   patch 'game/:id/result', controller: 'games', action: :update
 
