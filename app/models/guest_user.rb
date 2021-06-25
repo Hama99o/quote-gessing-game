c3f0963d7878c3b1ec927603c1b0c628ed3c0fc9
Hama <hmmshl@gmail.com>
2021-06-25 17:03:51 +0200

    email.updated

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -5,7 +5,7 @@ class GuestUser < ApplicationRecord
     GuestUser.create({
       slug: (0...50).map { ('a'..'z').to_a[rand(26)] }.join,
       name: 'guest',
-      email: 'guest@guotegame.com'
+      email: 'guest@game.com'
     })
   end
 end
