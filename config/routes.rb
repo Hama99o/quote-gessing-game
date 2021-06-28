774d53a8bd7364600115df84169c2e997edc9d50
Hama <hmmshl@gmail.com>
2021-06-28 16:09:24 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # resources :games, only: [:index, :show, :update]
-  # root to: "games#index"
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   root to: "games#home", as: 'home'
   get 'game', to: "games#index", as: 'games'
   get 'game/:id/result', controller: 'games', action: :result, as: 'game'
   patch 'game/:id/result', controller: 'games', action: :update
-
   get 'profile', controller: 'games', action: :profile, as: 'profile'
 end
