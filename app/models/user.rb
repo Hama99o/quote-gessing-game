30b070553d5aa83c6f35a988cee721922de8a493
Hama <hmmshl@gmail.com>
2021-06-28 19:25:04 +0200

    after_update_path_for method added to resgistrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 
   def self.merge_game_record(guest_slug, current_user)
     guest_user = GuestUser.find_by(slug: guest_slug )
-    Game.where(person_type: guest_user.class.to_s, person_id: guest_user.id).update_all(person_type: current_user.class.to_s, person_id: current_user.id)
+    Game.where(person_type: guest_user.class.to_s, person_id: guest_user.id)
+      .update_all(person_type: current_user.class.to_s, person_id: current_user.id)
   end
 
   has_many :games, as: :person
