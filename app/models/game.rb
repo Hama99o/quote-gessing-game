11070e3769328fae456461e8947c04faaafc4be4
Hama <hmmshl@gmail.com>
2021-06-28 11:11:11 +0200

    belong to added in game controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,14 +1,14 @@
 class Game < ApplicationRecord
   include Hashid::Rails
   before_save :set_has_guessed
-  # belongs_to :person, polymorphic: true
+  belongs_to :person, polymorphic: true
 
   # validates_uniqueness_of :api_id
   def set_has_guessed
     self.has_guessed = author == chosen_author if chosen_author.present?
   end
 
-  def self.generate_game(user_id, user_type)
+  def self.generate_game(person)
     quote_api = QuoteApi.new
     random_quote = quote_api.random_quote
     author = random_quote['quoteAuthor']
@@ -21,8 +21,7 @@ class Game < ApplicationRecord
       author: author,
       fake_authors: fake_authors,
       api_id: quote_id,
-      person_id: user_id,
-      person_type: user_type
+      person: person
     })
   end
 
