77c1e236be849cd59ff4c5da602fda89943bdc1e
hama <hmmshl@gmail.com>
2021-12-19 10:52:28 +0100

    password_confirmation added

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -30,7 +30,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
   end
 
   # If you have extra params to permit, append them to the sanitizer.
